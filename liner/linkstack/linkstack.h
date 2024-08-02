bcbbecff3f9d5f776c7bc55cc1d73454a727c2da
zhangmingling <mingling.zhang@outlook.com>
2024-08-02 16:48:25 +0800

    update

---
+++ liner/linkstack/linkstack.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int data_t;
+typedef struct node
+{
+    data_t data;
+    struct node *next;
+} listnode, *linkstack;
+
+linkstack stack_create();
+int stack_push(linkstack s, data_t value);
+data_t stack_pop(linkstack s);
+int stack_empty(linkstack s);
+data_t stack_top(linkstack s);
+linkstack stack_free(linkstack s);
