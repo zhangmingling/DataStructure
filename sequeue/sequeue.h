6c1083e7d1b1cccaf4debbf6c1f0a80ff33a76ef
zhangmingling <mingling.zhang@outlook.com>
2024-08-08 16:00:23 +0800

    update

--- sequeue/sequeue.h
+++ sequeue/sequeue.h
@@ -1,2 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define N 64
+typedef int datatype;
+typedef struct
+{
+    datatype data[N];
+    int front, rear;
+} sequeue;
+
+sequeue *queue_create();
+int enqueue(sequeue *sq, datatype x);
+datatype dequeue(sequeue *sq);
+int queue_empty(sequeue *sq);
+int queue_full(sequeue *sq);
+int queue_clear(sequeue *sq);
+sequeue *queue_free(sequeue *sq);
